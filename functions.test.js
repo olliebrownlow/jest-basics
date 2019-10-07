c9638a27272d392b3430a7338a1dbd7253467f41
olliebrownlow <olliebrownlow@gmail.com>
2019-10-07 22:25:58 +0100

    adds and passes 2nd test

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const functions = require('./functions');
 test('adding 2 + 2 equals 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test('adding 2 + 3 does not equal 4', () => {
+  expect(functions.add(2, 3)).not.toBe(4);
+});
