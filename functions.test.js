bdb61357f895e3a611725dbadc5b9f35b243a9e6
olliebrownlow <olliebrownlow@gmail.com>
2019-10-07 22:40:22 +0100

    adds test for toBeNull matcher

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,14 @@ test('adding 2 + 2 equals 4', () => {
 test('adding 2 + 3 does not equal 4', () => {
   expect(functions.add(2, 3)).not.toBe(4);
 });
+
+// MATCHERS TO CHECK FOR TRUTHY AND FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+test('should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
