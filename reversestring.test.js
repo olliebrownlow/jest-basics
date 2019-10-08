47001a722362ec09315601827ec4969510d004d0
olliebrownlow <olliebrownlow@gmail.com>
2019-10-08 13:31:49 +0100

    adds new test suite for reverseString, passes 2 tests

---
+++ reversestring.test.js
@@ -0,0 +1,9 @@
+const reverseString = require('./reversestring');
+
+test('reverseString function exists', () => {
+  expect(reverseString).toBeDefined();
+});
+
+test('reverseString reverses a string', () => {
+  expect(reverseString('hello')).toEqual('olleh');
+});
