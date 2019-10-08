e596e1a5e22a802bd5167bee7630de1cf32f1290
olliebrownlow <olliebrownlow@gmail.com>
2019-10-08 11:22:32 +0100

    passes test for toEqual matcher

--- functions.test.js
+++ functions.test.js
@@ -1,8 +1,18 @@
 const functions = require('./functions');
 
+// toBe works for primitive types only
 test('adding 2 + 2 equals 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+// toEqual works in other cases
+test('user should be Ollie Brownlow', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Ollie',
+    lastName: 'Brownlow'
+  });
+});
+
 
 test('adding 2 + 3 does not equal 4', () => {
   expect(functions.add(2, 3)).not.toBe(4);
